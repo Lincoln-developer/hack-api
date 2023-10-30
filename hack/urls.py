e6cca88136cd3f04319f210e5d60a7337f9b5f65
Lincoln-developer <angufibolinc@gmail.com>
2023-10-30 13:37:06 +0300

    added custom user creation

--- hack/urls.py
+++ hack/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for api_main project.
+URL configuration for hack project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls')),
+    path('auth/', include('hack_auth.urls')),
 ]
