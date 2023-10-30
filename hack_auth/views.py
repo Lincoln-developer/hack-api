c1a81e426cd637ddb55fffd07cf046a12a316d60
Lincoln-developer <angufibolinc@gmail.com>
2023-10-30 14:09:23 +0300

    added user account verification

--- hack_auth/views.py
+++ hack_auth/views.py
@@ -8,6 +8,8 @@ from .models import User
 from .utils import Util
 from django.contrib.sites.shortcuts import get_current_site
 from django.urls import reverse
+import jwt
+from django.conf import settings
 # Create your views here.
 
 class RegisterView(generics.GenericAPIView):
@@ -36,5 +38,18 @@ class RegisterView(generics.GenericAPIView):
 
 class VerifyEmailView(generics.GenericAPIView):
 
-    def verify_email(self):
-        pass
+    def verify_email(self, request):
+        token = request.GET.get("token")
+        try:
+            payload = jwt.decode(token, settings.SECRET_KEY)
+            user = User.objects.get(id=payload["user_id"])
+            if not user.is_verified:
+                user.is_verified = True
+                user.save()
+            return Response({"email":"Successfully activated"},
+                            status=status.HTTP_200_OK)
+        except jwt.ExpiredSignatureError as identifier:
+            return Response({"error":"Activation link has expired"},
+                            status=status.HTTP_400_BAD_REQUEST)
+        except jwt.exceptions.DecodeError as identifier:
+            return Response({"error":"Invalid token"}, status=status.HTTP_400_BAD_REQUEST)
